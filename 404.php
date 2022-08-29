13bfb82869b69fe1a8d3c201c95b2832aa6debfe
Daniel Honus <dhonus@tuta.io>
2022-08-29 10:56:12 +0200

    404 fix

--- 404.php
+++ 404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -10,51 +11,21 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ortodoncieostrava' ); ?></h1>
-			</header><!-- .page-header -->
+    <section class="error-404 not-found " style="width:50%; margin:auto; padding:4rem 0; text-align:center;">
+        <header class=" page-header" style="display:flex; justify-content:center;">
+            <h1 class="page-title"><?php esc_html_e('Stránka nebyla nalezena', 'ortodoncieostrava'); ?></h1>
+        </header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ortodoncieostrava' ); ?></p>
+        <div class="page-content">
+            <p><?php esc_html_e('Vámi hledaná stránka nebyla nalezena. Možná byla odebrána, přejmenována, nebo neexistuje.', 'ortodoncieostrava'); ?>
+            </p>
 
-					<?php
-					get_search_form();
+        </div><!-- .page-content -->
+    </section><!-- .error-404 -->
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ortodoncieostrava' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$ortodoncieostrava_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ortodoncieostrava' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ortodoncieostrava_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_footer();
